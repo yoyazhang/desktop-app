9fa3b0ac59db436db1f3bd0e222baecebd582dca
2449278392@qq.com <55231475+yoyazhang@users.noreply.github.com>
2022-03-31 09:55:38 +0800

    third version, can go left and right

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 const {app, BrowserWindow} = require('electron');
 function createWindow(){
     let win = new BrowserWindow({
-        width: 800,
+        width: 500,
         height: 600,
-        resizable: false,
-        backgroundColor: '#ffffff',
+        title: "goose",
+        transparent: true,// 透明窗口
+        alwaysOnTop: true, // 总是在最前面
+        // resizable: false,// 会导致不能拖动，所以不能用
         frame: false,
         icon: "icon/goose.png",
         webPreferences:{
             backgroundThrottling: false,   //设置应用在后台正常运行
-            nodeIntegration:true,     //设置能在页面使用nodejs的API
-            contextIsolation: false,  //关闭警告信息
+            nodeIntegration: true, //设置开启nodejs环境
+            contextIsolation: false,
+            enableRemoteModule: true, //允许子进程调用主进程的api
+            nativeWindowOpen:true,
+            webgl:true,
+            webSecurity: false
         }
     });
     // 让主进程加载一个index.html
--- renderer.js
+++ renderer.js
@@ -1,2 +1,9 @@
-let dino = document.getElementById('dino');
-dino.onclick
+let dino = document.getElementsByClassName("goose")[0];
+setInterval(function f() {
+        if (dino.getAttribute("id") === "left"){
+            dino.setAttribute("id", "right");
+        }else{
+            dino.setAttribute("id", "left");
+        }
+    }, 50000);
+
